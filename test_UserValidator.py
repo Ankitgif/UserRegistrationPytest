376993c64e77ea44e6abb24b1d62c689d6c758de
Ankitgif <ankitsinha2303@gmail.com>
2020-04-14 21:48:16 +0530

    [Ankit] Added User validator test file for the first name

---
+++ test_UserValidator.py
@@ -0,0 +1,9 @@
+import pytest
+from UserValidator import UserValidator
+class Test_UserValidatorTest:
+
+    def test_givenFirstName_WhenProper_ShouldReturnTrue(self):
+        validator = UserValidator()
+        result = validator.validateFirstName("Ankit")
+        assert result == 'matched'
+    
