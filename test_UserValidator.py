d1d7bfd474a0b9497ec16d88c17cf52860e98afe
Ankitgif <ankitsinha2303@gmail.com>
2020-04-14 22:42:06 +0530

    [Ankit] Added test user validator for last name

--- test_UserValidator.py
+++ test_UserValidator.py
@@ -7,3 +7,9 @@ class Test_UserValidatorTest:
         result = validator.validateFirstName("Ankit")
         assert result == 'matched'
 
+
+    def test_givenLastName_WhenProper_ShouldReturnTrue(self):
+        validator = UserValidator()
+        result = validator.validateLastName("Kumar")
+        assert result == 'matched'    
+
